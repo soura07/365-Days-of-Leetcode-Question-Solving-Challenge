1ae3aa0e506b23f9b08df869230234fd69ee7750
Souradeep Ghosh <116977697+soura07@users.noreply.github.com>
2024-04-30 00:26:52 +0530

    Time: 113 ms (46.77%) | Memory: 91.3 MB (54.80%) - LeetSync

---
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int totalXor = 0;
+        for(int &num : nums){
+          totalXor = totalXor ^ num;
+        }
+
+        int diff = (totalXor ^ k);
+
+        return __builtin_popcount(diff);
+    }
+};
