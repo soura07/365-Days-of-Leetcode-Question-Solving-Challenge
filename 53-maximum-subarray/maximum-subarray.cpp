124a83c4727e4253cb4003ec2e1973db648c1387
Souradeep Ghosh <116977697+soura07@users.noreply.github.com>
2024-05-03 14:36:23 +0530

    Time: 63 ms (94.75%) | Memory: 70 MB (92.78%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -7,13 +7,18 @@ public:
         ll sum = 0;
         for(int i=0; i<n; i++){
             sum = sum + nums[i];
-            if(sum>maxi){
+
+            if(sum > maxi){
                 maxi = sum;
             }
-            if(sum<0){
+
+            if(sum < 0){
                 sum = 0;
             }
+            // maxi = max(sum, maxi);
+            
         }
         return maxi;
+        
     }
 };
