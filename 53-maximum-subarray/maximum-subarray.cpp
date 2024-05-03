e46254049e6fa05ac713196af0dfbfc59bec8a21
Souradeep Ghosh <116977697+soura07@users.noreply.github.com>
2024-05-03 14:02:57 +0530

    Time: 80 ms (45.26%) | Memory: 70.2 MB (29.24%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    typedef long long ll;
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        ll maxi = LONG_MIN;
+        ll sum = 0;
+        for(int i=0; i<n; i++){
+            sum = sum + nums[i];
+            if(sum>maxi){
+                maxi = sum;
+            }
+            if(sum<0){
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
+};
