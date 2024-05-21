d6b2ce9180e972fcd7e08d13169a21a4d726c0c0
Souradeep Ghosh <116977697+soura07@users.noreply.github.com>
2024-05-21 21:50:30 +0530

    Time: 22 ms (81.08%) | Memory: 16 MB (51.92%) - LeetSync

---
+++ 18-4sum/4sum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        if(nums.empty()){
+            return ans;
+        }
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        for(int i=0; i<n;i++){
+            for(int j= i+1; j<n;j++){
+                long long target_2 = (long long)target - nums[j] - nums[i];
+                int front = j + 1;
+                int back = n - 1;
+
+                while(front < back){
+                    int two_sum = nums[front] + nums[back];
+
+                    if(two_sum < target_2) front++;
+                    
+                    else if(two_sum > target_2) back--;
+
+                    else{
+                        vector<int> quodruplet(4, 0);
+                        quodruplet[0] = nums[i];
+                        quodruplet[1] = nums[j];
+                        quodruplet[2] = nums[front];
+                        quodruplet[3] = nums[back];
+                        ans.push_back(quodruplet);
+
+                        while(front < back && nums[front] == quodruplet[2]) ++front;
+
+                        while(front < back && nums[back] == quodruplet[3]) --back;
+
+                    }
+
+                }
+
+                while(j + 1 < n && nums[j+1] == nums[j]) ++j;
+            }
+
+            while(i + 1 < n && nums[i+1] == nums[i]) ++i;
+        }
+        return ans;
+    }
+};
