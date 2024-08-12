002af8ec1cd41bb21fa0327ff877311033dc2b56
Souradeep Ghosh <116977697+soura07@users.noreply.github.com>
2024-08-13 02:07:08 +0530

    Time: 21 ms (89.60%) | Memory: 24.8 MB (17.50%) - LeetSync

---
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,25 @@
+
+class KthLargest {
+public:
+    int k;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    int add(int val) {
+        if (minHeap.size() < k) {
+            minHeap.push(val);
+        } else if (val > minHeap.top()) {
+            minHeap.pop();
+            minHeap.push(val);
+        }
+        
+        return minHeap.top();
+    }
+};
