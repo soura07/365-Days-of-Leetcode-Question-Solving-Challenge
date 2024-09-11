9216222935bea94de022280396941bd765adb446
Souradeep Ghosh <116977697+soura07@users.noreply.github.com>
2024-09-11 22:34:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (81.24%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -5,7 +5,7 @@ public:
         int cnt = 0;
         for(int i=0; i<31; i++){
             if(ans & (1 << i)){
-                cnt = cnt + 1;
+                cnt++;
             }
         }
         return cnt;
