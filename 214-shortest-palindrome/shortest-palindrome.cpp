30c2a0941ed4ec556101d4af680d64787b137932
Souradeep Ghosh <116977697+soura07@users.noreply.github.com>
2024-09-21 01:13:00 +0530

    Time: 3 ms (95.99%) | Memory: 12 MB (27.29%) - LeetSync

---
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    // Function to compute the Longest Prefix-Suffix (LPS) array
+    vector<int> LPS(string pat) {
+        int n = pat.size();
+        vector<int> lps(n, 0);
+        int i = 1, j = 0;
+
+        while (i < n) {
+            if (pat[i] == pat[j]) {
+                lps[i++] = ++j;
+            } else {
+                if (j == 0) {
+                    lps[i++] = 0;
+                } else {
+                    j = lps[j - 1];
+                }
+            }
+        }
+        return lps;
+    }
+
+    string shortestPalindrome(string s) {
+        if (s.empty()) return "";
+
+        string revs = s;
+        reverse(revs.begin(), revs.end());
+        string combined = s + "#" + revs;
+
+ 
+        vector<int> lps = LPS(combined);
+
+        int len = lps.back();
+        string to_add = s.substr(len);  
+        reverse(to_add.begin(), to_add.end());
+
+        return to_add + s; 
+    }
+};
