bcac65cacba6268e371b36888445874a6d1d56de
Souradeep Ghosh <116977697+soura07@users.noreply.github.com>
2024-09-23 21:07:23 +0530

    Time: 101 ms (48.28%) | Memory: 61.2 MB (61.41%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
-    int n;
-    bool isPalindrome(string &s, int l, int r){
-        while(l < r){
-            if(s[l] != s[r]){
-                return false;
-               
-            }
-            l++;
-            r--;
-            
-        }
-        return true;
+    vector<vector<string>> partition(string s) {
+        vector<vector<string> > ans;
+        vector<string> path;
+        palindromicFun(0, s, path, ans);
+        return ans;
     }
-
-
-    void backtrack(string &s, int idx, vector<string> &curr, vector<vector<string>> &result){
-        if(idx == n){
-            result.push_back(curr);
+    
+    void palindromicFun(int idx, string s, vector<string> &path, vector<vector<string> > &ans){
+        if(idx == s.size()){
+            ans.push_back(path);
             return;
         }
-
-        for(int i=idx; i<n; i++){
-            if(isPalindrome(s, idx, i)){
-                curr.push_back(s.substr(idx, i - idx + 1));
-
-                backtrack(s, i+1, curr, result);
-
-                curr.pop_back();
+        for(int i= idx; i<s.size(); ++i){
+            if(isPalindrome(s, idx,i)){
+                path.push_back(s.substr(idx, i - idx + 1));
+                palindromicFun(i+1, s, path, ans);
+                path.pop_back();
             }
         }
     }
-    vector<vector<string>> partition(string s) {
-        n = s.length();
-        vector<vector<string>> result;
-        vector<string> curr;
-        
-        backtrack(s,0,curr, result);
-        return result;
+    bool isPalindrome(string s, int start, int end){
+        while(start <= end){
+            if(s[start++] != s[end--]){
+                return false;
+            }
+        }
+        return true;
     }
 };
