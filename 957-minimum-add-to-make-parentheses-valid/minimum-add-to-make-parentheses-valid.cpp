22e910dabcd57a1e3eec621dccf960ac9eb9dc75
Souradeep Ghosh <116977697+soura07@users.noreply.github.com>
2024-10-09 21:46:20 +0530

    Time: 3 ms (42.94%) | Memory: 7.8 MB (81.46%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int open = 0, close = 0;
+        for(char ch : s){
+            if(ch == '('){
+                open++;
+            }
+            else if(open != 0){
+                open--;
+            }
+            else{
+                close++;
+            }
+        }
+        return close + open;
+    }
+};
