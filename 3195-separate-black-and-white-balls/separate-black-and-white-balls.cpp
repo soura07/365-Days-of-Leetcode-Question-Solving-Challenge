4ee366a7bf28e6dafa7be11eac7476db1db5cef1
Souradeep Ghosh <116977697+soura07@users.noreply.github.com>
2024-10-16 01:07:48 +0530

    Time: 36 ms (30.89%) | Memory: 15.7 MB (14.04%) - LeetSync

---
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long swap = 0;
+        int black = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s[i] == '0'){
+                swap = swap + (long long) black;
+            }
+            else{
+                black++;
+            }
+        }
+        return swap;
+    }
+};
