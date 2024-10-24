41ae3b178977cca496a4e9f7672502106ae9224b
Souradeep Ghosh <116977697+soura07@users.noreply.github.com>
2024-10-25 02:52:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (40.61%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double ans = 1.0;
-        long long nn = n;
-        if(nn < 0)
-            nn = -1 * nn;
-
-        while(nn){
-            if(nn%2){
-                ans = ans * x;
-                nn = nn - 1;
-            }
-            else{
-                x = x * x;
-                nn = nn/2;
-            }
+        if(n == 0) return 1.0;
+        if(x == 0) return 0.0;
+        if(x == 1) return 1.0;
+        if(x == -1 && n%2 == 0) return 1.0;
+        if(x == -1 && n%2 != 0) return -1.0;
+        long binform = n;
+        if(n < 0){
+            x = 1/x;
+            binform = -binform;
         }
-        if(n<0)
-            ans = (double)(1.0) / (double)(ans);
-            return ans;
+        double ans = 1;
+
+        while(binform > 0){
+            if(binform % 2 == 1){
+                ans = ans * x;
+            }
+
+            x = x*x;
+            binform = binform/2;
+        }
+        return ans;
     }
 };
